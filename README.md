f6939d85240254d1a9a3e414323e273f5c197936
Fanis Dokianakis <fanis@infloop.gr>
2016-03-24 22:34:52 +0200

    First implementation

---
+++ README.md
@@ -0,0 +1,10 @@
+-- simple stdin to xmpp-post-msg client
+
+Create a conf.lua file with the following 3 lines
+--
+local jid="user@example.com"
+local passwd="pass"
+local url="http://xmpphost:5280/msg"
+-- 
+
+To use give the receiving JID as an argument.
---
+++ xmpp-sieve-notify.lua
@@ -0,0 +1,33 @@
+-- 
+local io=require "io"
+local http=require "socket.http"
+local ltn12=require "ltn12"
+local mime=require "mime"
+
+local conf=require "conf"
+
+-- vars
+
+local jid=conf.jid
+local passwd=conf.jid
+local url=conf.url
+
+-- arg check
+if #arg ~= 1 then
+    error("wrong number of arguments")
+end
+
+if not string.match(arg[1],"^%w+@%w+%.%w+") or not string.match(arg[1],"^%w+@%w+%.%w+/%w+$") then
+    error("argument "..arg[1].." is not a jid")
+end
+
+local receiver=arg[1]
+local body=io.read("*all")
+
+local _,c = http.request{url=url.."/"..receiver, method="POST", headers={["Content-Type"] = "text/plain", ["Content-Length"] = body:len(), ["Authorization"] = "Basic "..(mime.b64(jid..":"..passwd)) }, source=ltn12.source.string(body) }
+
+if c == 201 then
+    do return end
+else
+    error("invalid status code")
+end
